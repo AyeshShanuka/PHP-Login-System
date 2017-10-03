92bdc8fd50830d2db9d4f86aef6c2a20902ba641
AyeshShanuka <32023892+AyeshShanuka@users.noreply.github.com>
2017-10-03 23:18:26 +0530

    Added config; added login/reg pages; added footer

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// If theres no constant var called __CONFIG__ , do not load this file
+	if(!defined('__CONFIG__')) {
+		exit("You do not have a config file");
+	}
+
+	// Allow the footer
+	define('ALLOW_FOOTER', true);
+
+	// Config is below
+
+?>
